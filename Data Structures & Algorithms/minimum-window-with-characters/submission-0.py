49b80c03dc0e0d92ddf163e69d3140c1583df303
Rui Xing <im.xingrui@gmail.com>
2026-09-13 13:17:18 +0800

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # two pointers
+        # the substring of s has at least frequent char as t, so when we move r, record char freq
+        # if the freq exceed, move l and recalculate the length
+        # return smallest length
+        if t == "":
+            return ""
+        # freq map of string t and current window count
+        countT =defaultdict(int)
+        window = defaultdict(int)
+        for c in t:
+            countT[c] += 1
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] += 1
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
